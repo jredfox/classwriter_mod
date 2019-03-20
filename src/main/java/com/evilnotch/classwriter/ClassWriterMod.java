b356cd0fe474928bd66f1f4907420a223f02c036
jredfox <jjredfox@hotmail.com>
2019-03-20 08:46:56 -0500

    Update ClassWriterMod.java

--- src/main/java/com/evilnotch/classwriter/ClassWriterMod.java
+++ src/main/java/com/evilnotch/classwriter/ClassWriterMod.java
@@ -3,7 +3,7 @@ package com.evilnotch.classwriter;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(name = "Class Writer Mod", modid = "mcclasswriter", version = "1.0")
+@Mod(name = "Class Writer Mod", modid = "mcclasswriter", version = "1.2")
 public class ClassWriterMod {
 
 }
